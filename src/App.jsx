9ca039538a916e6b16842adc7d6d9ff09c3adde1
Kennedy Owusu <kennediowusu@gmail.com>
2023-06-08 16:10:57 +0000

    use layout to fix the header issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
 import Vans from './pages/Vans/Vans';
 
 import { makeServer } from './server/index';
 import VanDetail from './pages/Vans/VanDetail';
+import Layout from './components/Layout';
 
 makeServer();
 
@@ -13,18 +14,13 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        <header>
-          <Link className='site-logo' to="/">#VanLife</Link>
-          <nav>
-            <Link to="/about">About</Link>
-            <Link to="/vans">Vans</Link>
-          </nav>
-        </header>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/vans" element={<Vans />} />
-          <Route path='/vans/:id' element={<VanDetail />} />
+          <Route element={<Layout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/vans" element={<Vans />} />
+            <Route path='/vans/:id' element={<VanDetail />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
