8fcd3020eb067fdbd47e7e4711e4e869665ba602
Kennedy Owusu <kennediowusu@gmail.com>
2023-06-08 19:11:08 +0000

    add active link styling with nav link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,23 @@
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const Header = () => {
   return (
     <header>
       <Link className='site-logo' to="/">#VanLife</Link>
       <nav>
-        <Link to="/host">Host</Link>
-        <Link to="/about">About</Link>
-        <Link to="/vans">Vans</Link>
+        <NavLink
+          className={({ isActive }) => isActive ? 'active-link' : ''}
+          to="/host">Host</NavLink>
+
+        <NavLink
+          className={({ isActive }) => isActive ? 'active-link' : ''}
+          to="/about">About</NavLink>
+
+        <NavLink
+          className={({ isActive }) => isActive ? 'active-link' : ''}
+          to="/vans">Vans</NavLink>
       </nav>
-   </header>
+    </header>
   )
 }
 
--- src/components/HostLayout.jsx
+++ src/components/HostLayout.jsx
@@ -1,12 +1,28 @@
-import { Link, Outlet } from 'react-router-dom'
+import { NavLink, Outlet } from 'react-router-dom'
 
 const HostLayout = () => {
+
+  const activeLinkStyle = {
+   fontWeight: "bold",
+   textDecoration: "underline",
+   color: "#161616"
+  }
+
  return (
    <>
    <nav className='host-nav'>
-     <Link to="/host">Dashboard</Link>
-     <Link to='/host/income'>Income</Link>
-     <Link to='/host/reviews'>Reviews</Link>
+    <NavLink
+     style={({ isActive }) => isActive ? activeLinkStyle : null}
+     end
+     to="/host">Dashboard</NavLink>
+
+    <NavLink
+      style={({isActive}) => isActive ? activeLinkStyle : null }
+     to='/host/income'>Income</NavLink>
+
+     <NavLink
+      style={({isActive}) => isActive ? activeLinkStyle : null }
+     to='/host/reviews'>Reviews</NavLink>
    </nav>
    <Outlet />
   </>
