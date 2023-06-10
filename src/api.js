781d776b609fed3f68507331a1e3bbe47dae281c
Kennedy Owusu <kennediowusu@gmail.com>
2023-06-10 05:48:04 +0000

    update api js file

--- src/api.js
+++ src/api.js
@@ -2,15 +2,15 @@ import axios from "axios";
 
 export async function getVans() {
   try {
-    const res = await axios.get("/api/vans");
-    if (res.status !== 200) {
+    const response = await axios.get("/api/vans");
+    if (response.status !== 200) {
       throw {
         message: "Failed to fetch vans",
-        statusText: res.statusText,
-        status: res.status,
+        statusText: response.statusText,
+        status: response.status,
       };
     }
-    const data = res.data;
+    const data = response.data;
     return data.vans;
   } catch (error) {
     console.error("Error retrieving vans:", error);
